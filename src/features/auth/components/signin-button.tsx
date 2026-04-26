cc5503dea40a712a517d67739ba90a8f9bde0bdc
ferdie bergado <ferdiebergado@gmail.com>
2026-04-26 16:36:26 +0800

    refactor: Add google icon

--- src/features/auth/components/signin-button.tsx
+++ src/features/auth/components/signin-button.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button';
+import { RiGoogleLine } from '@remixicon/react';
 import { genGoogleAuthUrl } from '..';
 
 export default function SigninButton() {
   return (
     <Button size="lg" onClick={() => (globalThis.location.href = genGoogleAuthUrl())}>
-      Continue with Google
+      <RiGoogleLine data-icon="inline-start" /> Continue with Google
     </Button>
   );
 }
