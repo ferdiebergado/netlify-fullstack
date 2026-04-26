2ef758c0437db89e634dde60f067f4404f329635
ferdie bergado <ferdiebergado@gmail.com>
2026-04-26 13:46:44 +0800

    refactor: Add pages

---
+++ src/app/home.tsx
@@ -0,0 +1,29 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+export default function Home() {
+  return (
+    <section>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="font-heading p-3 text-2xl font-semibold">Home</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Hello World!</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </section>
+  );
+}
