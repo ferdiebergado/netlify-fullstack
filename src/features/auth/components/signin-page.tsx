90560b9f70c4e53c983491eb4f9a1b8712d38e05
ferdie bergado <ferdiebergado@gmail.com>
2026-04-26 16:39:53 +0800

    refactor: Improve layout

--- src/features/auth/components/signin-page.tsx
+++ src/features/auth/components/signin-page.tsx
@@ -1,10 +1,17 @@
+import { Card, CardContent } from '@/components/ui/card';
 import SigninButton from './signin-button';
 
 export default function SigninPage() {
   return (
-    <section className="flex flex-col items-center gap-5">
-      <h1 className="font-heading text-2xl font-bold">Signin</h1>
-      <SigninButton />
+    <section>
+      <Card className="mx-auto max-w-sm p-10">
+        <CardContent className="flex flex-col items-center gap-10">
+          <h1 className="text-center text-xl font-semibold text-balance">
+            Sign in to your Account
+          </h1>
+          <SigninButton />
+        </CardContent>
+      </Card>
     </section>
   );
 }
