1e739931776ac7f182d40e05522d25ec7eb5ea00
ferdie bergado <ferdiebergado@gmail.com>
2026-04-26 16:21:55 +0800

    feat: Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@/components/ui/button';
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty';
+import { RiCloseLargeLine } from '@remixicon/react';
+import { useNavigate } from 'react-router';
+
+export default function PageNotFound() {
+  const navigate = useNavigate();
+
+  const handleHome = () => navigate('/');
+  const handleBack = () => navigate(-1);
+
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <RiCloseLargeLine />
+        </EmptyMedia>
+        <EmptyTitle className="text-3xl font-semibold">Page not found</EmptyTitle>
+        <EmptyDescription>The page you're looking for doesn't exist.</EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent className="flex-row justify-center gap-2">
+        <Button onClick={handleHome}>Home</Button>
+        <Button variant="outline" onClick={handleBack}>
+          Go back
+        </Button>
+      </EmptyContent>
+    </Empty>
+  );
+}
