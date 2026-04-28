f401bf7f0ac200ec8bdbadcc820e8c618de22c95
ferdie bergado <ferdiebergado@gmail.com>
2026-04-28 21:06:54 +0800

    refactor: Set background for dark mode

--- src/components/guest-layout.tsx
+++ src/components/guest-layout.tsx
@@ -3,7 +3,7 @@ import Footer from './footer';
 
 export default function GuestLayout() {
   return (
-    <div className="flex h-dvh flex-col bg-neutral-50">
+    <div className="flex h-dvh flex-col bg-neutral-50 dark:bg-neutral-950">
       <main className="flex flex-1 flex-col items-center justify-center p-3">
         <Outlet />
       </main>
