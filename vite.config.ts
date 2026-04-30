b246f3ce1965ba9c85e4b21d4caadedba909f019
ferdie bergado <ferdiebergado@gmail.com>
2026-04-30 05:31:28 +0000

    chore: Setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,33 @@
+/// <reference types="vitest/config" />
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import path from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+
+const dirname = import.meta.dirname;
+
+const alias = {
+  '@': path.resolve(dirname, './src'),
+  '@shared': path.resolve(dirname, './shared'),
+};
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@shared': path.resolve(__dirname, './shared'),
-    },
+    alias,
+  },
+  test: {
+    projects: [
+      {
+        test: {
+          name: 'unit',
+          include: ['tests/unit/**/*.{test,spec}.ts', 'tests/integration/**/*.{test,spec}.ts'],
+          alias,
+          environment: 'node',
+        },
+      },
+    ],
   },
 });
