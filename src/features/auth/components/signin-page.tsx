21ea777619ddb018e52857cea6f2c62a25f9d016
ferdie bergado <ferdiebergado@gmail.com>
2026-05-01 14:29:38 +0800

    refactor: Update SigninPage layout and welcome message

--- src/features/auth/components/signin-page.tsx
+++ src/features/auth/components/signin-page.tsx
@@ -1,11 +1,14 @@
 import { Card, CardContent } from '@/components/ui/card';
+import config from '@/config';
 import SigninButton from './signin-button';
 
 export default function SigninPage() {
   return (
-    <Card className="mx-auto max-w-sm p-10">
+    <Card className="mx-auto max-w-3xl px-6 py-10">
       <CardContent className="flex flex-col items-center gap-10">
-        <h1 className="text-center text-xl font-semibold text-balance">Sign in to your Account</h1>
+        <h1 className="text-center text-3xl font-semibold text-balance">
+          Welcome to {config.appTitle}
+        </h1>
         <SigninButton />
       </CardContent>
     </Card>
