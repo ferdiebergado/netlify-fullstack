6437a1255b8a5450dde03539a0eaad8ebafbbf4e
ferdie bergado <ferdiebergado@gmail.com>
2026-05-01 17:19:39 +0800

    refactor: Set data-icon attribute

--- src/components/sort-button.tsx
+++ src/components/sort-button.tsx
@@ -12,7 +12,7 @@ export default function SortButton<T>({ column, children }: SortButtonProps<T>) 
   return (
     <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
       {children}
-      <RiArrowUpDownFill className="ml-2 size-4" />
+      <RiArrowUpDownFill className="ml-2 size-4" data-icon="inline-end" />
     </Button>
   );
 }
