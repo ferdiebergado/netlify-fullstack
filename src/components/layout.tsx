22672e3d710c8d8c38b0e8927dd6383512fc70db
ferdie bergado <ferdiebergado@gmail.com>
2026-05-05 08:43:13 +0000

    refactor: Move suspense boundary to the layout component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,18 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router';
+
 import Footer from './footer';
 import Header from './header';
+import SkeletonPage from './skeleton-page';
 
 export default function Layout() {
   return (
-    <div className="flex h-dvh flex-col bg-background">
+    <div className="bg-background flex h-dvh flex-col">
       <Header />
       <main className="mx-auto w-full max-w-5xl flex-1 p-3">
-        <Outlet />
+        <Suspense fallback={<SkeletonPage />}>
+          <Outlet />
+        </Suspense>
       </main>
       <Footer />
     </div>
