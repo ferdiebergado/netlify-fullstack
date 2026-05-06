79f1679ded1d38efb667a4d2f3f376596472249e
ferdie bergado <ferdiebergado@gmail.com>
2026-05-06 01:30:27 +0000

    refactor: Decrease bottom margin

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-background mb-15 w-full shadow">
+    <header className="bg-background mb-5 w-full shadow">
       <div className="flex items-center justify-between px-8 py-4 md:mx-auto md:max-w-5xl">
         <h1 className="font-heading text-2xl font-bold">App</h1>
 
