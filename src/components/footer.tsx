66106e9f905f3d20f26d4eabd531da82ccf367e2
ferdie bergado <ferdiebergado@gmail.com>
2026-05-06 01:30:01 +0000

    refactor: Change 'p' tag to 'footer'

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <p className="text-muted-foreground p-5 text-center text-sm ">
+    <footer className="text-muted-foreground bg-secondary p-5 text-center text-sm">
       &copy; 2026 to present
-    </p>
+    </footer>
   );
 }
