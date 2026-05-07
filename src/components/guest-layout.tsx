76742ece0f8e4e383fe16ab0310287192a7b5bd3
ferdie bergado <ferdiebergado@gmail.com>
2026-05-07 03:47:13 +0000

    refactor: Remove header div

--- src/components/guest-layout.tsx
+++ src/components/guest-layout.tsx
@@ -1,13 +1,9 @@
 import { Outlet } from 'react-router';
 import Footer from './footer';
-import Header from './header';
 
 export default function GuestLayout() {
   return (
     <div className="bg-secondary flex h-dvh flex-col">
-      <div className="pointer-events-none opacity-0">
-        <Header />
-      </div>
       <main className="flex w-full flex-1 items-center justify-center p-3 md:mx-auto md:max-w-5xl">
         <Outlet />
       </main>
